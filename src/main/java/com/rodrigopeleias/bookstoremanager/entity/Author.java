0df516f9554a27b504893594b046d5d055272f64
uislancruz <uislansk8@hotmail.com>
2022-07-22 18:49:03 -0300

    Crianto as classes e seus  metodos do projeto

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Author.java
@@ -0,0 +1,17 @@
+package com.rodrigopeleias.bookstoremanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Book.java
@@ -0,0 +1,18 @@
+package com.rodrigopeleias.bookstoremanager.entity;
+
+public class Book {
+
+    private Long id;
+
+    private String name;
+
+    private Integer pages;
+
+    private Integer chapter;
+
+    private String isbn;
+
+    private String publisherName;
+
+    private Author author;
+}
