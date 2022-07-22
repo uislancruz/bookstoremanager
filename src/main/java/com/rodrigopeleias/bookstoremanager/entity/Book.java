998b49ae2650faa53b438b40cba49d872eda4c53
uislancruz <uislansk8@hotmail.com>
2022-07-22 19:19:09 -0300

    Fazendo o mapeamento das entidades

--- src/main/java/com/rodrigopeleias/bookstoremanager/entity/Book.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Book.java
@@ -1,18 +1,30 @@
 package com.rodrigopeleias.bookstoremanager.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String name;
 
+    @Column(nullable = false)
     private Integer pages;
 
+    @Column(nullable = false)
     private Integer chapter;
 
+    @Column(nullable = false)
     private String isbn;
 
+    @Column(name = "publisher_name",nullable = false, unique = true)
     private String publisherName;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
     private Author author;
 }
