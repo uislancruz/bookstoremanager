a3d3f4c36c13dcb45ba70273935af504d06779bf
uislancruz <uislansk8@hotmail.com>
2022-07-27 20:31:23 -0300

    Criando classe de mapeamento, e para configurar o biuld

--- src/main/java/com/rodrigopeleias/bookstoremanager/dto/BookDTO.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/dto/BookDTO.java
@@ -1,10 +1,17 @@
 package com.rodrigopeleias.bookstoremanager.dto;
 
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -12,15 +19,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BookDTO {
 
+    private Long id;
+
+    @NotBlank
+    @Size(max = 200)
     private String name;
 
+    @NotNull
     private Integer pages;
 
+    @NotNull
     private Integer chapter;
 
+    @NotBlank
+    @Size(max = 100)
+    @Pattern(regexp = "(?:ISBN(?:-10)?:?)?(?=[0-9x]{10}$|(?=(?:[0-9]+[- ])))",
+            message = "ISBN format must be a valid format")
     private String isbn;
 
+    @NotBlank
+    @Size(max = 200)
     private String publisherName;
 
+    @Valid
+    @NotNull
     private AuthorDTO author;
 }
--- src/main/java/com/rodrigopeleias/bookstoremanager/service/BookService.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/service/BookService.java
@@ -1,24 +1,30 @@
 package com.rodrigopeleias.bookstoremanager.service;
 
+import com.rodrigopeleias.bookstoremanager.dto.BookDTO;
 import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
 import com.rodrigopeleias.bookstoremanager.entity.Book;
+import com.rodrigopeleias.bookstoremanager.mapper.BookMapper;
 import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Service
 public class BookService {
 
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+
+    private final BookMapper bookMapper = BookMapper.INSTANCE;
 
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public MessageResponseDTO create(Book book){
-        Book savedBook = bookRepository.save(book);
+    public MessageResponseDTO create(BookDTO bookDTO){
+        Book bookTosave = bookMapper.toModel(bookDTO);
+
+        Book savedBook = bookRepository.save(bookTosave);
         return MessageResponseDTO.builder().massage("Book created with ID").build();
     }
 
