d8b1acc202345b84b1e9377ef4dd53e8709b588d
uislancruz <uislansk8@hotmail.com>
2022-07-26 22:42:41 -0300

    Refatorando e criando o livro

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/service/BookService.java
@@ -0,0 +1,25 @@
+package com.rodrigopeleias.bookstoremanager.service;
+
+import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
+import com.rodrigopeleias.bookstoremanager.entity.Book;
+import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public MessageResponseDTO create(Book book){
+        Book savedBook = bookRepository.save(book);
+        return MessageResponseDTO.builder().massage("Book created with ID").build();
+    }
+
+}
