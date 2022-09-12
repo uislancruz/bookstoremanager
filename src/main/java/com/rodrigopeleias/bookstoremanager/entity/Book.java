308377f41188932959f4c0179266ebe642de97ed
uislancruz <uislansk8@hotmail.com>
2022-09-12 19:29:38 -0300

    Testando a conexão

--- src/main/java/com/rodrigopeleias/bookstoremanager/entity/Book.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Book.java
@@ -17,7 +17,7 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    //testando GIT
     @Column(nullable = false, unique = true)
     private String name;
 
